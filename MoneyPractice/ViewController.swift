adb3e6f24de4cc5387d79ed3acb9d5f2d3600dc1
krauserhuang <crescentdence@gmail.com>
2020-09-29 17:00:28 +0800

    Initial Commit

---
+++ MoneyPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyPractice
+//
+//  Created by Tai Chin Huang on 2020/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
